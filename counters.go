0eae5fea1a6c381415913d39ef4006eca1098ce9
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-03-28 11:07:22 +1030

    Add Int32, Int64, Uint32 and Uint64 counter types

---
+++ counters.go
@@ -0,0 +1,10 @@
+// Copyright 2017 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License that can be found in
+// the LICENSE file.
+
+// Package counters implements atomic counters for statistics
+// collection.
+package counters
+
+//go:generate go run generate.go
