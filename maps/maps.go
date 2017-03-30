6eac6a4a216253f2a0aee55c0ad551fc59b5310d
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-03-30 15:10:44 +1030

    Add atomic maps using golang.org/x/sync/syncmap

---
+++ maps/maps.go
@@ -0,0 +1,9 @@
+// Copyright 2017 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License that can be found in
+// the LICENSE file.
+
+// Package maps implements maps of atomic types.
+package maps
+
+//go:generate go run generate-map.go
